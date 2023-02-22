07ee02679261626638df8475a53c1ccdf3cbf42b
shayoni saha <123863457+shayonisahatuktuky@users.noreply.github.com>
2023-02-22 23:47:13 +0530

    Create Information.java

---
+++ Information.java
@@ -0,0 +1,11 @@
+package information;
+
+public class Information {
+
+    public static void main(String[] args) {
+     String name ="Shayoni Saha Tuktuky",ID ="221-15-4791";
+     String address ="RASG",department="CSE",favgame="Football";
+     System.out.println("Name:"+name +"\n"+"ID:"+ID +"\n"+"Address:"+address +"\n"+"Department:"+department +"\n"+"Favorite game:"+favgame );
+    }
+    
+}
