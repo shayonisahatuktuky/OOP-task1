7f65c0275fc8ae8f886992c2eaee983748e2c329
shayoni saha <123863457+shayonisahatuktuky@users.noreply.github.com>
2023-02-22 23:04:25 +0530

    Create calculation.java

---
+++ calculation.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+      int A=8,B=9,C=7,D=10;
+      System.out.println("(A*B-C*D)="+(A*B-C*D));
+      System.out.println("(2*A-B+3*D)="+(2*A-B+3*D));
+      System.out.println("(A^2+B^2-C^2+D^2)="+(A*A+B*B-C*C+D*D));
+      System.out.println("(A^3+B-C^2)="+(A*A*A+B-C*C*C));
+    }
+    
+}
